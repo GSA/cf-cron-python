8c7cd07507890fa93c4affefca6caeef2cfa1d13
csmcallister <scott.mcallister@gsa.gov>
2018-12-19 13:50:12 -0500

    fix typos

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def print_something_now():
     now = datetime.now()
     
     print(f"something logged at: {now}.\n")
-    if env = os.getenv('VCAP_APPLICATION'):
+    env = os.getenv('VCAP_APPLICATION')
+    if env:
         print(env)
     else:
         print("No env vars inside this container...")
