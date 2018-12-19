bf047112b18859cc89d3aa463ec9c2b0921257e1
csmcallister <scott.mcallister@gsa.gov>
2018-12-19 13:47:52 -0500

    print cf env vars

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from datetime import datetime
+import os
 
 def print_something_now():
 
     now = datetime.now()
-    print(f"something logged at: {now}")
+    
+    print(f"something logged at: {now}.\n")
+    if env = os.getenv('VCAP_APPLICATION'):
+        print(env)
+    else:
+        print("No env vars inside this container...")
 
 if __name__ == '__main__':
     print_something_now()
